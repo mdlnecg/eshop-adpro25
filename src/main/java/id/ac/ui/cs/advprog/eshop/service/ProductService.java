a343da397e7a0ebfa78af889dcd70ac1d65cbf2d
mdlnecg <mdlnecg@gmail.com>
2025-02-11 10:52:07 +0700

    Add ProductService.java and ProductServiceImpl.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -1,4 +1,9 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
-public class ProductService {
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import java.util.List;
+
+public interface ProductService {
+    public Product create(Product product);
+    public List<Product>findAll();
 }
