9aa4411726eff132575c5555b8714dbe620a1081
mdlnecg <mdlnecg@gmail.com>
2025-02-20 12:55:44 +0700

    Code coverage 100%

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -138,4 +138,75 @@ public class ProductRepositoryTest {
         Iterator<Product> productIterator = productRepository.findAll();
         assertFalse(productIterator.hasNext());
     }
+
+    @Test
+    void testFoundProductByID() {
+        Product product = new Product();
+        product.setProductId("123");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+        productRepository.create(product);
+
+        Product foundProduct = productRepository.findById("123");
+        assertNotNull(foundProduct);
+        assertEquals("123", foundProduct.getProductId());
+        assertEquals("Sampo Cap Bambang", foundProduct.getProductName());
+        assertEquals(100, foundProduct.getProductQuantity());
+    }
+
+    @Test
+    void testNotFoundProductByID() {
+        Product foundProduct = productRepository.findById("999");
+        assertNull(foundProduct);
+    }
+
+    @Test
+    void testNullProductID() {
+        Product product = new Product();
+        product.setProductId(null);
+        productRepository.create(product);
+
+        Product foundProduct = productRepository.findById("123");
+        assertNull(foundProduct);
+    }
+
+    @Test
+    void testEditProductIdShouldNotChange() {
+        Product product = new Product();
+        product.setProductId("456");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+        productRepository.create(product);
+
+        Product updatedProduct = new Product();
+        updatedProduct.setProductName("Sampo Cap Anto");
+        updatedProduct.setProductQuantity(50);
+        productRepository.edit(product.getProductId(), updatedProduct);
+
+        Product foundProduct = productRepository.findById("456");
+        assertNotNull(foundProduct);
+        assertEquals("456", foundProduct.getProductId());
+        assertEquals("Sampo Cap Anto", foundProduct.getProductName());
+        assertEquals(50, foundProduct.getProductQuantity());
+    }
+
+    @Test
+    void testDeleteIfProductExists_VerifyNotDeletedAccidentally() {
+        Product product1 = new Product();
+        product1.setProductId("111");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(100);
+        productRepository.create(product1);
+
+        Product product2 = new Product();
+        product2.setProductId("222");
+        product2.setProductName("Sampo Cap Usep");
+        product2.setProductQuantity(50);
+        productRepository.create(product2);
+
+        productRepository.delete("111");
+
+        assertNull(productRepository.findById("111"));
+        assertNotNull(productRepository.findById("222"));
+    }
 }
